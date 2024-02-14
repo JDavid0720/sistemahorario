5b4a708305c0d82d1d1a054870dec371d54fed2f
persontechnology <persontechnologys@gmail.com>
2024-02-14 10:56:43 -0500

    cambio de carpeta

--- modules/conexion.php
+++ modules/conexion.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('America/Mexico_City');
 
-$conexion = mysqli_connect("localhost", "root", "", "utchorario");
+$conexion = mysqli_connect("localhost", "sistemahorario", "sistemahorario", "sistemahorario");
 
 if (mysqli_connect_errno()) {
 	printf("Falló la conexión: %s\n", mysqli_connect_error());
